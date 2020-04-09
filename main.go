8d76674dacad9823eeac579b9e61314c9c3cd69c
Elozzy <st.eloghosa@gmail.com>
2020-04-09 12:49:02 +0100

    adding codes to wrote

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import(
 	"encoding/json"
 	"log"
 	"net/http"
-	"math/rand"
-	"strconv" 
+	// "math/rand"
+	// "strconv" 
 	"github.com/gorilla/mux"
 )
 
@@ -19,21 +19,45 @@ type Book struct {
 	
 }
 
+// Author Struct
+type Author struct {
+	Firstname string `json:"firstname "`
+	Lastname   string `json:"lastname  "`
+}
+
+
+// Init books var as a slice Book struct
+var books []Book
+
+
 
 // GET Al Books
 func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
 
 
 }
 
 //Get single book
 func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) //get params
+	// loop through books and find with id
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
 	
 
 }
 
 //Create Book
 func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	
 
 }
@@ -50,23 +74,23 @@ func deleteBook(w http.ResponseWriter, r *http.Request) {
 }
 
 
-// Author Struct
-type Author struct {
-	Firstname string `json:"firstname "`
-	Lastname   string `json:"lastname  "`
-}
+
 
 func main() {
 	// Init Router
-
 	r := mux.NewRouter()
+
+	// Hardcoded data 
+	books = append(books, Book{ID: "1", Isbn: "438227", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Doe"}})
+	books = append(books, Book{ID: "2", Isbn: "454555", Title: "Book Two", Author: &Author{Firstname: "jane", Lastname: "doe"}})
+
 
 	// route handlers /endpoints
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
 	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
-	r.HandleFunc("/api/books", createBooks).Methods("POST")
-	r.HandleFunc("/api/books{id}", updateBooks).Methods("PUT")
-	r.HandleFunc("/api/books/{id}", deleteBooks).Methods("DELETE")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 
 
 	//server
