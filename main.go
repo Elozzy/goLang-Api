a142236de010a04281c558527354b96790696e01
Elozzy <st.eloghosa@gmail.com>
2020-04-09 12:58:57 +0100

    completed the ceate book endpoint

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import(
 	"encoding/json"
 	"log"
 	"net/http"
-	// "math/rand"
-	// "strconv" 
+	"math/rand"
+	"strconv" 
 	"github.com/gorilla/mux"
 )
 
@@ -58,8 +58,11 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 //Create Book
 func createBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	
-
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(10000000))
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
 }
 
 // Update book
